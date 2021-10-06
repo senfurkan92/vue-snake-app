e23d25eac87377adb71a024acd4a27581237e3e6
senfurkan92 <senfurkan92@gmail.com>
2021-10-06 14:44:20 +0300

    commit second

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 // vue.config.js file to be place in the root of your repository
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/snake/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/vue-snake-app/' : '/',
 };
